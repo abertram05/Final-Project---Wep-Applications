cdc5cec673800b80d674d716e8c9be73595ab29b
abertram05 <[albertram@anderson.edu]>
2025-11-12 12:47:06 -0500

    working on challenge 1 and styling the index page

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function startChallenge(Challenge) {
-    
+// set the secret password to Cyber123! 
+// this is the password that the user will have to guess
+const secretPassword = "Cyber123!";
+
+// finds the button on the page
+const button = document.getElementById("guessButton");
+
+if (button) {
+    button.addEventListener("click", () => {
+        const userGuess = document.getElementById("guessInput").value.trim();
+        const result = document.getElementById("result");
+
+        if (userGuess === secretPassword) {
+            result.textContent = "Correct! You have found the correct password! Now go try challenge 2!"
+        } else {
+            result.textContent = "Incorrect! Keep looking for the clues!"
+        }
+    });
 }
